f1945c55a5e016d4145590d213ba57acdcc68a21
Taiki-Ishigaki <ishigaki@ynl.t.u-tokyo.ac.jp>
2024-07-07 11:06:07 +0900

    🌟ADD: basic func for jax

--- sui/basic.py
+++ sui/basic.py
@@ -4,8 +4,11 @@
 
 import numpy as np
 import sympy as sp
+import jax.numpy as jnp
 
 import math
+
+LIB_EPPOR = "Unsupported library. Choose 'numpy', 'sympy' or 'jax."
 
 def iszero(x):
   tolerance = 1e-8  # 許容範囲
@@ -16,16 +19,20 @@ def sin(theta, LIB = 'numpy'):
     return np.sin(theta)
   elif LIB == 'sympy':
     return sp.sin(theta)
+  elif LIB == 'jax':
+    return jnp.sin(theta)
   else:
-    raise ValueError("Unsupported library. Choose 'numpy' or 'sympy'.")
+    raise ValueError(LIB_EPPOR)
   
 def cos(theta, LIB = 'numpy'):
   if LIB == 'numpy':
     return np.cos(theta)
   elif LIB == 'sympy':
     return sp.cos(theta)
+  elif LIB == 'jax':
+    return jnp.cos(theta)
   else:
-    raise ValueError("Unsupported library. Choose 'numpy' or 'sympy'.")
+    raise ValueError(LIB_EPPOR)
 
 def zeros(shape, LIB = 'numpy'):
   if LIB == 'numpy':
@@ -35,24 +42,30 @@ def zeros(shape, LIB = 'numpy'):
       return sp.zeros(shape[0],shape[1])
     elif len(shape) == 1:
       return sp.vector.zeros(shape)
+  elif LIB == 'jax':
+    return jnp.zeros(shape)
   else:
-    raise ValueError("Unsupported library. Choose 'numpy' or 'sympy'.")
+    raise ValueError(LIB_EPPOR)
   
 def identity(size, LIB = 'numpy'):
   if LIB == 'numpy':
     return np.identity(size)
   elif LIB == 'sympy':
     return sp.identity(size)
+  elif LIB == 'jax':
+    return jnp.identity(size)
   else:
-    raise ValueError("Unsupported library. Choose 'numpy' or 'sympy'.")  
+    raise ValueError(LIB_EPPOR)
 
 def norm(vec, LIB = 'numpy'):
-    if LIB == 'numpy':
-      return np.linalg.norm(vec)
-    elif LIB == 'sympy':
-      return sp.sqrt(vec.dot(vec))
-    else:
-      raise ValueError("Unsupported library. Choose 'numpy' or 'sympy'.")
+  if LIB == 'numpy':
+    return np.linalg.norm(vec)
+  elif LIB == 'sympy':
+    return sp.sqrt(vec.dot(vec))
+  elif LIB == 'jax':
+    return jnp.linalg.norm(vec)
+  else:
+    raise ValueError(LIB_EPPOR)
 
 def jac_lie_wrt_scaler(lie, vec, a, dvec):
   m = lie.mat(vec, a)
